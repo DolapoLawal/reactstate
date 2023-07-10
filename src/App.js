251eead5f94d01505c734d8dff919f208dee8b37
DolapoLawal <daminat76@yahoo.com>
2023-07-10 16:43:17 +0100

    reactstate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        fullName: 'Dolapo lawal',
+        bio: 'Ceo Binary Cleantech limited',
+        imgSrc: 'images/oriamo2.jpeg',
+        profession: 'Food Lover'
+      },
+      show: false,
+      IntervalId: null,
+      timeElapsed: 0
+    };
+  };
+
+  componentDidMount() {
+    const IntervalId = setInterval(this.updateTimeElapsed, 1000);
+    this.setState({ IntervalId });
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.state.IntervalId);
+  };
+
+  updateTimeElapsed = () => {
+    this.setState((prevState) => ({
+      timeElapsed: prevState.timeElapsed + 1
+    }));
+  };
+
+  toggleShow = () => {
+    this.setState((prevState) => ({ show: !prevState.show }));
+  };
+
+  render() {
+    const { person, show, timeElapsed } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.toggleShow}>Toggle Show</button>
+        {show && (
+          <div>
+            <h2>{person.fullName}</h2>
+            <p>{person.bio}</p>
+            <img src={person.imgSrc} alt={person.fullName} />
+            <p>{person.profession}</p>
+          </div>
+        )}
+        <p>Time Elapsed: {timeElapsed} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
